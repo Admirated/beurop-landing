2478b669f5c977bbdc9e86f17f56dcc9e9a2cc5c
Vladimir <vovanebozhinsky@gmail.com>
2021-12-09 21:46:17 +0200

    navigation

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js';
+
+new Swiper('.card__slider', {
+   scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true
+   },
+   grabCursor: true,
+   slideToClickedSlide: true,
+   // mousewheel: {
+   //    sensitivity: 1,
+   //    eventsTarget: '.card__slider'
+   // },
+   slidesPerView: 1.5,
+   autoplay: {
+      delay: 1000
+   },
+   speed: 800,
+   initialSlide: 1,
+   centeredSlides: true,
+   breakpoints: {
+      776: {
+         slidesPerView: 2.5,
+         initialSlide: 0,
+         centeredSlides: false
+      }
+   },
+   loop: true
+});
+
 window.addEventListener('DOMContentLoaded', () => {
    const burger = document.querySelector('.menu__icon'),
       menu = document.querySelector('.header__navigation'),
